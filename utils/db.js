d050609d863a8ec8022c7b4973c2db8bdca68e2d
12 <twelvve@arch.localdomain>
2022-02-03 10:41:06 +0545

    -mongouri

--- utils/db.js
+++ utils/db.js
@@ -1,9 +1,6 @@
 import  mongoose  from "mongoose";
 
-const MONGOURI = "mongodb+srv://oneaboveall:awesome12@cluster0.dlvfe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-// const MONGOURI = process.env.MONGOURI  // does not work  ??? why idk
-
-// console.log(MONGOURI)
+const MONGOURI = process.env.MONGOURI 
 if (!MONGOURI) {
   throw new Error(
     'Please define the "MONGODB_URI" environment variable inside .env'
@@ -13,20 +10,19 @@ const connection = {};
 
 async function connect(){
     if (connection.isConnected){
-        console.log('already connected');
+        // console.log('already connected');
         return;
     }
     if(mongoose.connections.length> 0){
         connection.isConnected = mongoose.connections[0].readyState;
         if(connection.isConnected===1){
-            console.log('use previous connection');
+            // console.log('use previous connection');
             return;
         }
         await mongoose.disconnect();
     }
-    console.log("-----------------------")
     const db = await mongoose.connect(MONGOURI);
-    console.log("new connection");
+    // console.log("new connection");
     connection.isConnected = db.connections[0].readyState;
 }
 
